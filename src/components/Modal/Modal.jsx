bb943f29f623c48934b0a225de4ff73968c199ef
facundo2309 <facundoomargarcia@gmail.com>
2022-09-01 10:00:06 -0300

    clean code

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,26 +2,11 @@ import './style.css'
 import { AiFillCloseCircle} from 'react-icons/ai'
 
 const Modal=({data,handelShowModal})=>{
-    console.log(JSON.stringify(data, null, 2))
-    const showData=(data)=>{
-        let arrdata=[]
-        for(let val in data){
-            arrdata=[...arrdata,data[val]]
-        }
-        return arrdata
-    }
-    
     return(
         <article className='modal is-open'>
             <div className='modal-container'>
-              {/* {showData(data).map((e,index)=>{
-                return(
-                    <p key={index}>{e}</p>
-                )
-              })} */}
-              <div className='data'>{JSON.stringify(data, null, 2)}</div>
-            
-                <button className='modal-close'onClick={handelShowModal}><AiFillCloseCircle className='icon-modal'/></button>
+               <div className='data'>{JSON.stringify(data, null, 2)}</div>
+               <button className='modal-close'onClick={handelShowModal}><AiFillCloseCircle className='icon-modal'/></button>
             </div>
         </article>
     )
